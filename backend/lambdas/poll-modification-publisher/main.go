98504f186b8e8d8ea0309d9c1395a8152a00abb4
Declan Scott <dscott1008@gmail.com>
2023-12-28 23:54:53 -0600

    refactor(publisher): Refactor short circuit logic

--- backend/lambdas/poll-modification-publisher/main.go
+++ backend/lambdas/poll-modification-publisher/main.go
@@ -53,6 +53,11 @@ type PollModifiedPayload struct {
 }
 
 func handler(ctx context.Context, event events.CloudWatchEvent) {
+	if event.Source != os.Getenv("SOURCE") || event.DetailType != os.Getenv("DETAIL_TYPE") {
+		log.Printf("Unknown event source or detail type: %s, %s\n", event.Source, event.DetailType)
+		return
+	}
+
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Printf("Error: %s\n", err)
@@ -61,65 +66,60 @@ func handler(ctx context.Context, event events.CloudWatchEvent) {
 
 	iot := iotdataplane.NewFromConfig(cfg)
 
-	if event.Source == os.Getenv("SOURCE") && event.DetailType == os.Getenv("DETAIL_TYPE") {
-		var pollModifiedDetail PollModifiedDetail
-		if err := json.Unmarshal(event.Detail, &pollModifiedDetail); err != nil {
-			log.Printf("Error: %s\n", err)
-			return
-		}
-		log.Printf("Poll modified: %s\n", pollModifiedDetail.DynamoDb.NewImage.PollId.S)
+	var pollModifiedDetail PollModifiedDetail
+	if err := json.Unmarshal(event.Detail, &pollModifiedDetail); err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+	log.Printf("Poll modified: %s\n", pollModifiedDetail.DynamoDb.NewImage.PollId.S)
 
-		if pollModifiedDetail.DynamoDb.NewImage.Duration.N != pollModifiedDetail.DynamoDb.OldImage.Duration.N {
-			log.Printf(
-				"Duration changed: poll %s with %s seconds\n",
-				pollModifiedDetail.DynamoDb.NewImage.PollId.S,
-				pollModifiedDetail.DynamoDb.NewImage.Duration.N,
-			)
-
-			duration, err := strconv.ParseInt(pollModifiedDetail.DynamoDb.NewImage.Duration.N, 10, 64)
-			if err != nil {
-				log.Printf("Error: %s\n", err)
-				return
-			}
-
-			payload, err := json.Marshal(PollModifiedPayload{
-				PollId:    pollModifiedDetail.DynamoDb.NewImage.PollId.S,
-				UserId:    pollModifiedDetail.DynamoDb.NewImage.UserId.S,
-				Prompt:    pollModifiedDetail.DynamoDb.NewImage.Prompt.S,
-				CreatedAt: pollModifiedDetail.DynamoDb.NewImage.CreatedAt.S,
-				Duration:  duration,
-				Archived:  pollModifiedDetail.DynamoDb.NewImage.Archived.BOOL,
-			})
-			if err != nil {
-				log.Printf("Error: %s\n", err)
-				return
-			}
-
-			pieces := strings.Split(pollModifiedDetail.DynamoDb.NewImage.PollId.S, "poll|")
-			if len(pieces) != 2 {
-				log.Printf("Error: %s\n", err)
-				return
-			}
-			pollId := pieces[1]
-
-			_, err = iot.Publish(ctx, &iotdataplane.PublishInput{
-				Topic:       aws.String(pollId),
-				ContentType: aws.String("application/json"),
-				Payload:     payload,
-			})
-			if err != nil {
-				log.Printf("Error: %s\n", err)
-				return
-			}
-
-			return
-		}
-
+	if pollModifiedDetail.DynamoDb.NewImage.Duration == pollModifiedDetail.DynamoDb.OldImage.Duration {
+		log.Printf("Duration unchanged: poll %s\n", pollModifiedDetail.DynamoDb.NewImage.PollId.S)
 		return
 	}
 
-	log.Printf("Unknown event source or detail type: %s, %s\n", event.Source, event.DetailType)
-	return
+	log.Printf(
+		"Duration changed: poll %s with %s seconds\n",
+		pollModifiedDetail.DynamoDb.NewImage.PollId.S,
+		pollModifiedDetail.DynamoDb.NewImage.Duration.N,
+	)
+
+	duration, err := strconv.ParseInt(pollModifiedDetail.DynamoDb.NewImage.Duration.N, 10, 64)
+	if err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+
+	payload, err := json.Marshal(PollModifiedPayload{
+		PollId:    pollModifiedDetail.DynamoDb.NewImage.PollId.S,
+		UserId:    pollModifiedDetail.DynamoDb.NewImage.UserId.S,
+		Prompt:    pollModifiedDetail.DynamoDb.NewImage.Prompt.S,
+		CreatedAt: pollModifiedDetail.DynamoDb.NewImage.CreatedAt.S,
+		Duration:  duration,
+		Archived:  pollModifiedDetail.DynamoDb.NewImage.Archived.BOOL,
+	})
+	if err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+
+	pieces := strings.Split(pollModifiedDetail.DynamoDb.NewImage.PollId.S, "poll|")
+	if len(pieces) != 2 {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+	pollId := pieces[1]
+
+	_, err = iot.Publish(ctx, &iotdataplane.PublishInput{
+		Topic:       aws.String(pollId),
+		ContentType: aws.String("application/json"),
+		Payload:     payload,
+	})
+	if err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+
 }
 
 func main() {
--- backend/lambdas/vote-count-publisher/main.go
+++ backend/lambdas/vote-count-publisher/main.go
@@ -24,6 +24,9 @@ type VoteCountedDetail struct {
 			PollId struct {
 				S string `json:"S"`
 			} `json:"GSI1PK"`
+			Index struct {
+				N string `json:"N"`
+			} `json:"Index"`
 			Text struct {
 				S string `json:"S"`
 			} `json:"Text"`
@@ -45,6 +48,11 @@ type VoteCountedPayload struct {
 }
 
 func handler(ctx context.Context, event events.CloudWatchEvent) {
+	if event.Source != os.Getenv("SOURCE") || event.DetailType != os.Getenv("DETAIL_TYPE") {
+		log.Printf("Unknown event source or detail type: %s, %s\n", event.Source, event.DetailType)
+		return
+	}
+
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Printf("Error: %s\n", err)
@@ -53,57 +61,51 @@ func handler(ctx context.Context, event events.CloudWatchEvent) {
 
 	iot := iotdataplane.NewFromConfig(cfg)
 
-	if event.Source == os.Getenv("SOURCE") && event.DetailType == os.Getenv("DETAIL_TYPE") {
-		var voteCountedDetail VoteCountedDetail
-		if err := json.Unmarshal(event.Detail, &voteCountedDetail); err != nil {
-			log.Printf("Error: %s\n", err)
-			return
-		}
-		log.Printf(
-			"Vote counted: %s with %s votes\n",
-			voteCountedDetail.DynamoDb.NewImage.OptionId.S,
-			voteCountedDetail.DynamoDb.NewImage.Votes.N,
-		)
+	var voteCountedDetail VoteCountedDetail
+	if err := json.Unmarshal(event.Detail, &voteCountedDetail); err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+	log.Printf(
+		"Vote counted: %s with %s votes\n",
+		voteCountedDetail.DynamoDb.NewImage.OptionId.S,
+		voteCountedDetail.DynamoDb.NewImage.Votes.N,
+	)
 
-		votes, err := strconv.ParseInt(voteCountedDetail.DynamoDb.NewImage.Votes.N, 10, 64)
-		if err != nil {
-			log.Printf("Error: %s\n", err)
-			return
-		}
-
-		payload, err := json.Marshal(VoteCountedPayload{
-			OptionId: voteCountedDetail.DynamoDb.NewImage.OptionId.S,
-			PollId:   voteCountedDetail.DynamoDb.NewImage.PollId.S,
-			VotedAt:  voteCountedDetail.DynamoDb.NewImage.UpdatedAt.S,
-			Votes:    votes,
-		})
-		if err != nil {
-			log.Printf("Error: %s\n", err)
-			return
-		}
-
-		pieces := strings.Split(voteCountedDetail.DynamoDb.NewImage.PollId.S, "poll|")
-		if len(pieces) != 2 {
-			log.Printf("Error: %s\n", err)
-			return
-		}
-		pollId := pieces[1]
-
-		_, err = iot.Publish(ctx, &iotdataplane.PublishInput{
-			Topic:       aws.String(pollId),
-			ContentType: aws.String("application/json"),
-			Payload:     payload,
-		})
-		if err != nil {
-			log.Printf("Error: %s\n", err)
-			return
-		}
-
+	votes, err := strconv.ParseInt(voteCountedDetail.DynamoDb.NewImage.Votes.N, 10, 64)
+	if err != nil {
+		log.Printf("Error: %s\n", err)
 		return
 	}
 
-	log.Printf("Unknown event source or detail type: %s, %s\n", event.Source, event.DetailType)
-	return
+	payload, err := json.Marshal(VoteCountedPayload{
+		OptionId: voteCountedDetail.DynamoDb.NewImage.OptionId.S,
+		PollId:   voteCountedDetail.DynamoDb.NewImage.PollId.S,
+		VotedAt:  voteCountedDetail.DynamoDb.NewImage.UpdatedAt.S,
+		Votes:    votes,
+	})
+	if err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+
+	pieces := strings.Split(voteCountedDetail.DynamoDb.NewImage.PollId.S, "poll|")
+	if len(pieces) != 2 {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+	pollId := pieces[1]
+
+	_, err = iot.Publish(ctx, &iotdataplane.PublishInput{
+		Topic:       aws.String(pollId),
+		ContentType: aws.String("application/json"),
+		Payload:     payload,
+	})
+	if err != nil {
+		log.Printf("Error: %s\n", err)
+		return
+	}
+
 }
 
 func main() {
