16910a967580548f6ee14a395d042ce5c927e3d4
Declan Scott <dscott1008@gmail.com>
2024-01-25 13:36:27 -0600

    Use `cf-connecting-ip` instead of `x-real-ip`

--- frontend/server/api/polls/[pollId].delete.ts
+++ frontend/server/api/polls/[pollId].delete.ts
@@ -1,7 +1,6 @@
 import { safeParse } from "valibot";
 
 export default defineEventHandler(async (event) => {
-  console.log("DELETE /polls/{pollId}");
   console.log("event", event);
   console.log("headers", getHeaders(event));
   const session = await getServerAuthSession(event);
@@ -43,7 +42,6 @@ export default defineEventHandler(async (event) => {
     params: { path: routerParams.output },
     headers: {
       Authorization: `Bearer ${session.user.idToken}`,
-      "x-real-ip": getHeader(event, "x-real-ip") ?? "",
     },
     body: body.output,
   });
