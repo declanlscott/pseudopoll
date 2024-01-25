a308e430683053feded3d09749230ea6b050fb83
Declan Scott <dscott1008@gmail.com>
2024-01-25 12:47:29 -0600

    Logging in archive endpoint

--- frontend/server/api/polls/[pollId].delete.ts
+++ frontend/server/api/polls/[pollId].delete.ts
@@ -1,8 +1,11 @@
 import { safeParse } from "valibot";
 
 export default defineEventHandler(async (event) => {
+  console.log("DELETE /polls/{pollId}");
+  console.log("event", event);
   const session = await getServerAuthSession(event);
   if (!session) {
+    console.log("Unauthorized", session);
     throw createError({
       statusCode: 401,
       message: "Unauthorized",
@@ -10,10 +13,12 @@ export default defineEventHandler(async (event) => {
   }
 
   const config = useRuntimeConfig();
+  console.log("config", config);
   const routerParams = await getValidatedRouterParams(event, (params) =>
     safeParse(pollParamsSchema(config.public), params),
   );
   if (!routerParams.success) {
+    console.log("Bad request params", routerParams);
     throw createError({
       statusCode: 400,
       message: routerParams.issues.map((issue) => issue.message).join(". "),
@@ -24,6 +29,7 @@ export default defineEventHandler(async (event) => {
     safeParse(archiveSchema, body),
   );
   if (!body.success) {
+    console.log("Bad request body", body);
     throw createError({
       statusCode: 400,
       message: body.issues.map((issue) => issue.message).join(". "),
@@ -39,11 +45,12 @@ export default defineEventHandler(async (event) => {
     body: body.output,
   });
   if (result.error) {
+    console.log("API error", result.error);
     throw createError({
       statusCode: result.response.status,
       message: `${result.error.message}. ${result.error.cause}`,
     });
   }
-
+  console.log("API result", result.data);
   return result.data;
 });
